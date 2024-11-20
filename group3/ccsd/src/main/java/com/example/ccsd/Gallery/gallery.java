f828ff405398fa446f2977ffeabdb165eeabe43a
[juliziah] <[juliziah88@gmail.com]>
2024-11-20 16:51:18 +0800

    gallery added

---
+++ group3/ccsd/src/main/java/com/example/ccsd/Gallery/gallery.java
@@ -0,0 +1,15 @@
+package com.example.ccsd.Gallery;
+
+public class gallery {
+
+    private String logo;
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+}
