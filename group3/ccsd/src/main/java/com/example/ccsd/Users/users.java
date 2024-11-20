922de67774d379f12ef9cb2994e491c05e6cf89d
eddyallyson <95832783+eddyallyson@users.noreply.github.com>
2024-11-20 16:49:44 +0800

    user

--- group3/ccsd/src/main/java/com/example/ccsd/Users/users.java
+++ group3/ccsd/src/main/java/com/example/ccsd/Users/users.java
@@ -1,5 +1,87 @@
 package com.example.ccsd.Users;
 
 public class users {
+        private int userID;
+        private String FirstName;
+        private String LastName;
+        private String phoneNumber;
+        private String userEmail;
+        private String userPassword;
+        private String userimage;
+
+        public users(int userID, String firstName, String lastName, String phoneNumber, String userEmail, String userPassword, String userImage) {
+            this.userID =userID;
+            this.FirstName=firstName;
+            this.LastName=lastName;
+            this.phoneNumber=phoneNumber;
+            this.userEmail=userEmail;
+            this.userPassword=userPassword;
+            this.userimage=userImage;
+        }
+        public int getUserID() {
+            return userID;
+        }
     
-}
+        public void setUserID(int userID) {
+            this.userID = userID;
+        }
+    
+       
+        public String getFirstName() {
+            return FirstName;
+        }
+    
+        public void setFirstName(String firstName) {
+            this.FirstName = firstName;
+        }
+    
+       
+        public String getLastName() {
+            return LastName;
+        }
+    
+        public void setLastName(String lastName) {
+            this.LastName = lastName;
+        }
+    
+        
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+    
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    
+        
+        public String getUserEmail() {
+            return userEmail;
+        }
+    
+        public void setUserEmail(String userEmail) {
+            this.userEmail = userEmail;
+        }
+    
+      
+        public String getUserPassword() {
+            return userPassword;
+        }
+    
+        public void setUserPassword(String userPassword) {
+            this.userPassword = userPassword;
+        }
+    
+        
+        public String getUserImage() {
+            return userimage;
+        }
+    
+        public void setUserImage(String userImage) {
+            this.userimage = userImage;
+        }
+    }
+
+
+    
+
+
