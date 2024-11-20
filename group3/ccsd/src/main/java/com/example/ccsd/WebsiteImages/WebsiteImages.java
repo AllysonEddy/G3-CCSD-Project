71da9e3783da0a6829a7c627a1aac9a3e3e2acb5
Syafique-crz <fixcrew17@gmail.com>
2024-11-20 16:50:19 +0800

    webimage

--- group3/ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
+++ group3/ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
@@ -1,5 +1,30 @@
 package com.example.ccsd.WebsiteImages;
 
+import java.util.List;
+
 public class WebsiteImages {
-    
+    private List<String> foodImages;   // Store multiple image paths for food
+    private List<String> drinkImages; // Store multiple image paths for drinks
+    private List<String> specialImages; // Store multiple image paths for specials
+
+    public List<String> getFoodImages() {
+        return this.foodImages;
+    }
+    public void setFoodImages(List<String> foodImages) {
+        this.foodImages = foodImages;
+    }
+
+    public List<String> getDrinkImages() {
+        return this.drinkImages;
+    }
+    public void setDrinkImages(List<String> drinkImages) {
+        this.drinkImages = drinkImages;
+    }
+
+    public List<String> getSpecialImages() {
+        return this.specialImages;
+    }
+    public void setSpecialImages(List<String> specialImages) {
+        this.specialImages = specialImages;
+    }
 }
