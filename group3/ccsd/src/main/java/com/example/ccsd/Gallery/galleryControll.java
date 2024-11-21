07e165e7775557b807c4ed8ff0f7285a99b5e04b
[juliziah] <[juliziah88@gmail.com]>
2024-11-21 15:01:10 +0800

    change cr

--- group3/ccsd/src/main/java/com/example/ccsd/Gallery/galleryControll.java
+++ group3/ccsd/src/main/java/com/example/ccsd/Gallery/galleryControll.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/gallery")
-public class galleryController {
+public class galleryControll {
     @Autowired
     private galleryService galleryService;
 
