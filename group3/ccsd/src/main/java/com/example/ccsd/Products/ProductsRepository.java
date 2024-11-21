c26ddfeb0435bb764e277c61742f8991e7bc13f4
Syafique-crz <fixcrew17@gmail.com>
2024-11-21 14:49:13 +0800

    Removed products

--- group3/ccsd/src/main/java/com/example/ccsd/Products/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ccsd.Products;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductsRepository extends MongoRepository<Products, String> {
-    
-}
